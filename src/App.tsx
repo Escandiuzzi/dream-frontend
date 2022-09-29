567bf7830d2e6ab39583f61fdf19ddb6a813cbc2
Luiz Felipe Escandiuzzi <lfescandiuzzi@icloud.com>
2022-09-28 23:10:59 -0300

    front end changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+//import { io } from "socket.io-client";
+
+// const socket = io("http://localhost:3333", {
+//   withCredentials: true,
+// });
+
+// socket.on('connection', () => {
+//   console.log('connectd');
+// });
+
+// socket.on('start', () => { console.log('received message from game room') });
+// socket.on('role', (...args) => { console.log(args); socket.emit('guessed', '') });
+// socket.on("connect_error", (err: any) => {
+//   console.log(`connect_error due to ${err.message}`);
+// });
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="font-bold m-36 text-7xl font-sans">
+        When i dream...
+      </h1>
+
+      <div className='grid grid-cols-4 px-32'>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold mr-3 mt-3 py-2 px-4 rounded">
+          Join Room 1
+        </button>
+
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold mr-3 mt-3 py-2 px-4 rounded">
+          Join Room 2
+        </button>
+
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold mr-3 mt-3 py-2 px-4 rounded">
+          Join Luiz Felipe`s Room
+        </button>
+
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold mr-3 mt-3 py-2 px-4 rounded">
+          Join Dreaming
+        </button>
+        
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold mr-3 mt-3 py-2 px-4 rounded">
+          Join Brazil
+        </button>
+        
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold mr-3 mt-3 py-2 px-4 rounded">
+          Join NodeJS
+        </button>
+        
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold mr-3 mt-3 py-2 px-4 rounded">
+          Join Typescript
+        </button>
+        
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold mr-3 mt-3 py-2 px-4 rounded">
+          Join Another Room
+        </button>
+        
+      </div>
+
+
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold m-12 py-2 px-4 rounded">
+        Create Room
+      </button>
     </div>
   );
 }
---
+++ src/components/Room/Room.tsx
@@ -0,0 +1,11 @@
+import './Room.css';
+
+function Room() {
+  return (
+    <div className="Room">
+      <h1>Room 1</h1>
+    </div>
+  );
+}
+
+export default Room;
